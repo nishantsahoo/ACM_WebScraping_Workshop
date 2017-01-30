6f64b1d0c0bed3aaccdf8eb308a62881da921a0e
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-30 19:33:25 +0530

    Initial Commit

--- GetData_KG_Webpage.py
+++ GetData_KG_Webpage.py
@@ -5,6 +5,10 @@ sys.stdout = open('KG_Data.txt', 'w')
 url = "www.kashishgrover.com"
 r = urllib3.PoolManager().request('GET', url).data
 soup = BeautifulSoup(r, "html.parser")
-print soup.find('title')
-print soup.find('title').text
-
+exp = soup.find('div', attrs={'class':'flexin'})
+shippable = exp.find('div', attrs={'id':'shippable'})
+blurb = shippable.find('div', attrs={'class':'blurb'})
+ul = blurb.find('ul')
+list = ul.findAll('li')
+for each in list:
+    print each.text
