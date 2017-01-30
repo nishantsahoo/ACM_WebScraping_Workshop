ed2a38ee4eacb2a4d4ce5f08677c6cfae5601f63
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-30 15:03:35 +0530

    Initial Commit

---
+++ GetData_KG_Webpage.py
@@ -0,0 +1,9 @@
+import urllib3
+import sys
+from bs4 import BeautifulSoup
+sys.stdout = open('KG_Data.txt', 'w')
+url = "www.kashishgrover.com"
+r = urllib3.PoolManager().request('GET', url).data
+soup = BeautifulSoup(r, "html.parser")
+print soup.find('title')
+print soup.find('title').text
