1b570afbf9c2c4175f60954e15b8fcbad094f21d
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-30 14:54:40 +0530

    Initial Commit

--- ScrapeURL.py
+++ ScrapeURL.py
@@ -4,5 +4,6 @@ from bs4 import BeautifulSoup
 sys.stdout = open('ScrapeURL.txt', 'w')
 url = "www.kashishgrover.com"
 r = urllib3.PoolManager().request('GET', url).data  # http://urllib3.readthedocs.io/en/latest/user-guide.html#response-content
-soup = BeautifulSoup(r, "html.parser")
+soup = BeautifulSoup(r, "html.parser")  # Beautiful soup object
 print soup
+# urllib3.PoolManager().request('GET', url).data => Returns an HTTPResponse object whose data is stored in a variable
