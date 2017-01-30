278fa7a558bb383bd106831b2c7edaaab4bf1f9b
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-30 14:52:53 +0530

    Initial Commit

--- ScrapeURL.py
+++ ScrapeURL.py
@@ -3,6 +3,6 @@ import sys
 from bs4 import BeautifulSoup
 sys.stdout = open('ScrapeURL.txt', 'w')
 url = "www.kashishgrover.com"
-r = urllib3.PoolManager().request('GET', url).data
+r = urllib3.PoolManager().request('GET', url).data  # http://urllib3.readthedocs.io/en/latest/user-guide.html#response-content
 soup = BeautifulSoup(r, "html.parser")
 print soup
